029f82a3988ef91f4b1ff190719cb969fc5445ab
titus-gitau <titus.gitau@outlook.com>
2025-09-06 13:26:34 +0300

    WEEK 5 ASSIGNMENT SUBMISSION

---
+++ answers.sql
@@ -0,0 +1,11 @@
+-- Use the salesdb database
+USE salesdb;
+
+
+DROP INDEX IdxPhone ON customers;
+
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+GRANT INSERT ON salesdb.* TO 'bob'@'localhost';
+
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
